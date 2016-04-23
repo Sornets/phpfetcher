4cf85923a1da478b357d80d35d08f68bcb50d125
root <root@iZ28wmzvetcZ.(none)>
2016-04-23 17:43:08 +0800

    merge pc & host

--- Phpfetcher/MySQL/Default.php
+++ Phpfetcher/MySQL/Default.php
@@ -1,28 +1,28 @@
 <?
-$config_tpl = array(
+/*$config_tpl = array(
 	'db_host' 		=> '',
 	'db_port'		=> '3306',
 	'db_username' 	=> '',
 	'db_password' 	=> '',
 	'db_name' 		=> '',//库名
 	'db_pre' 		=> '',//前缀
-);
-class MySQL{
+);*/
+class Phpfetcher_MySQL_Default{
 	private $_con;//连接
 	public $_pre = '';
 	public $_db_name = '';
-	private function __construct( $config ){
+	public function __construct( $config ){
 		//判断$config数组是否符合规范
-		foreach( $config_tpl as $key => $value ){
+		/*foreach( $GLOBALS['config_tpl'] as $key => $value ){
 			if(!isset( $config[ $key ] ) ){
 				Phpfetcher_Log::warning( "bad \$config" );
 			}
-		}
+		}*/
 		//创建数据库连接并保存
 		$this->_con = mysql_connect( 
-			$config['db_host'] . ':' . $config['port'],
-			$config['username'],
-			$config['password']
+			$config['db_host'] . ':' . $config['db_port'],
+			$config['db_username'],
+			$config['db_password']
 		);
 		$this->_pre = $config['db_pre'];
 		$this->_db_name = $config['db_name'];
@@ -31,7 +31,7 @@ class MySQL{
 		}
 	}
 
-	private function __destruct(){
+	public function __destruct(){
 		mysql_close($_con);
 	}
 
