b5ee9f257290cfcb03cd9c7d560ace67f4b5005c
suxici <hellosxc@163.com>
2016-05-13 18:49:02 +0800

    完善获取评论功能，传到服务器上debug

--- demo/qqnews.php
+++ demo/qqnews.php
@@ -21,12 +21,6 @@ class mycrawler extends Phpfetcher_Crawler_Default {
     public function handlePage($page) {
         //获取标题
         $str_title = $page->sel('//title', 0)->plaintext;
-        /*if(	PATH_SEPARATOR == ':' ){
-			echo $str_title . PHP_EOL;
-		}
-		else{
-			echo iconv("UTF-8", "GBK", $str_title);
-		}*/
 		//获取文章内容
         $arr_content = $page->sel('div[@id=Cnt-Main-Article-QQ]/p');
         if( $arr_content ){
@@ -35,38 +29,80 @@ class mycrawler extends Phpfetcher_Crawler_Default {
 				$str_content .= $content->plaintext;
       	     	$str_content .= "<br />";
 			}
-			//echo "文章段落数：" . count( $arr_content );
-			//获取评论数
-			
-			//$obj_comment = $page->sel('span[@class=r all-number-comment]/a', 0);
+			//获取评论info
+			//获取文章id
 			$str_html = $page->getContent();
 			$str_cmt_id_patten = "#cmt_id\s?=\s?\d*;#";
 			preg_match( $str_cmt_id_patten, $str_html, $matches );
 			$matches[0] = str_replace( "cmt_id = ", "", $matches[0] );
 			$matches[0] = str_replace( ";", "", $matches[0] );
 			$cmt_id = intval( $matches[0] );
-			var_dump( $matches );
-			var_dump( $cmt_id );
-			
-			//$news_id = intval( str_replace( "http://coral.qq.com/", "", $obj_comment->href ) );
-			/*$comment_url = "http://coral.qq.com/article/$news_id/commentnum";
-			
-			// 设置你需要抓取的URL
-			curl_setopt($GLOBALS['curl'], CURLOPT_URL, $comment_url);
-			 
-			// 设置header
-			curl_setopt($GLOBALS['curl'], CURLOPT_HEADER, 1);
-			 
-			// 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
-			curl_setopt($GLOBALS['curl'], CURLOPT_RETURNTRANSFER, 1);
-			 
-			// 运行cURL，请求网页
-			$str_json = curl_exec($GLOBALS['curl']);
-			$arr_json = json_decode($str_json, TRUE);
-			$int_comment = intval( $arr_json['data']['commentnum'] );
-			$int_orgcomment = intval( $arr_json['data']['orgcommentnum'] );
-			*/
-			$int_comment = 0;
+			$error_count = 0;
+
+			if( $cmt_id ){
+				$next_cmt_id = 0;
+				$comment_url = "http://coral.qq.com/article/$news_id/comment";
+				//循环获取json评论
+				do{
+					// 设置你需要抓取的URL
+					curl_setopt($GLOBALS['curl'], CURLOPT_URL, $comment_url . "?commentid=$next_cmt_id&reqnum=20&callback=mainComment");
+					 
+					// 设置header
+					curl_setopt($GLOBALS['curl'], CURLOPT_HEADER, 1);
+					 
+					// 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
+					curl_setopt($GLOBALS['curl'], CURLOPT_RETURNTRANSFER, 1);
+					 
+					// 运行cURL，请求网页
+					$str_json = curl_exec($GLOBALS['curl']);
+					$arr_json = json_decode($str_json, TRUE);
+					if( $arr_json && $arr_json['errCode'] == 0 ){
+						$next_cmt_id = $arr_json['data']['last'];//获取成功即修改下一次的参数
+						$get_time = $arr_json['info']['time'];
+						$error_count = 0;
+						foreach( $arr_json['data']['commentid'] as $comment ){
+							$user = $comment['userinfo'];
+							$weibo = $comment['userinfo']['wbuserinfo'];
+							$str_comment_sql = "INSERT INTO `comments`(`id`, `rootid`, `targetid`, `parent`, `timeDifference`, `time`, `content`, `title`, `up`, `rep`, `type`, `hotscale`, `checktype`, `checkstatus`, `isdeleted`, `tagself`, `taghost`, `source`, `location`, `address`, `rank`, `custom`, `extend_at`, `extend_ut`, `orireplynum`, `richtype`, `userid`, `poke`, `abstract`, `thirdid`, `replyuser`, `replyuserid`, `replyhwvip`, `replyhwlevel`, `replyhwannual`, `remark`, `fnd`, `create_time`, `update_time`, `analysis_time`) VALUES ($comment[id], $comment[rootid], $comment[targetid], $comment[parent], '$comment[timeDifference]', $comment[time], '$comment[content]', '$comment[title]', $comment[up], $comment[rep], $comment[type], $comment[hotscale], $comment[checktype], $comment[checkstatus], $comment[isdeleted], '$comment[tagself]', '$comment[taghost]', $comment[source], '$comment[location]', '$comment[address]', $comment[rank], '$comment[custom]', {$comment['extend']['at']}, {$comment['extend']['ut'], $comment[orireplynum], $comment[richtype], $comment[userid], $comment[poke], '$comment[abstract]', '$comment[thirdid]', '$comment[replyuser]', $comment[replyuserid], $comment[replyhwvip], $comment[replyhwlevel], $comment[replyhwannual], '$comment[remark]', $comment[fnd], $get_time, $get_time, 0 )";
+							$GLOBALS['db']->exe_sql( $str_comment_sql );
+
+							$str_user_sql = "INSERT INTO `users`(
+											`userid`, `uidex`, `nick`, `head`, 
+											`gender`, `viptype`, `mediaid`, `region`, 
+											`thirdlogin`, `hwvip`, `hwlevel`, 
+											`hwannual`, `identity`, `wb_name`, 
+											`wb_nick`, `wb_url`, `wb_vip`, `wb_ep`, 
+											`wb_brife`, `wb_identification`, 
+											`wb_intro`, `wb_live_country`, `wb_live_province`, 
+											`wb_live_city`, `wb_live_area`, 
+											`wb_gender`, `wb_level`, `wb_classify`
+										) 
+										VALUES 
+											(
+												$user[userid], '$user[uidex]', '$user[nick]', '$user[head]', 
+												$user[gender], $user[viptype], $user[mediaid], 
+												'$user[region], $user[thirdlogin], $user[hwvip], 
+												$user[hwlevel], $user[hwannual], $user[identity], 
+												'$weibo[name]', '$weibo[nick]', '$weibo[url]', 
+												$weibo[vip], $weibo[ep], '$weibo[brief]', 
+												'$weibo[identification]', '$weibo[intro]', {$weibo['liveaddr']['country']}, 
+												{$weibo['liveaddr']['province']}, {$weibo['liveaddr']['city']}, {$weibo['liveaddr']['area']}, 
+												$weibo[gender], $weibo[level], '$weibo[classify]'
+											)";
+							$GLOBALS['db']->exe_sql( $str_user_sql);
+						}
+					}
+					else{
+						$error_count++;
+						if( $error_count > 10 ){
+							break;
+						}
+						continue;
+					}
+				}while( $arr_json['data']['hasnext'] );
+
+				$int_comment = $arr_json['data']['total'];
+			}
 			//获取新闻类型
 			@$str_type = $page->sel('span[@bosszone=ztTopic]/a', 0)->plaintext;
 			
@@ -102,10 +138,10 @@ class mycrawler extends Phpfetcher_Crawler_Default {
 				$check_sql = "SELECT id FROM `news` WHERE title='$str_title'";
 				if( !$GLOBALS['db']->exe_sql( $check_sql ) ){
 					Phpfetcher_Log::warning("insert into mysql failed!");
-					mysql_select_db( 'fail', $GLOBALS['db']->_con );	
+					//mysql_select_db( 'fail', $GLOBALS['db']->_con );	
 					$error_sql = "INSERT INTO `fail`( `content` ) VALUES ( '". mysql_real_escape_string($sql)  . "')";
 					$GLOBALS['db']->exe_sql( $error_sql );
-					mysql_select_db( $GLOBALS['db']->_db_name, $GLOBALS['db']->_con );
+					//mysql_select_db( $GLOBALS['db']->_db_name, $GLOBALS['db']->_con );
 					echo $str_title;
 				}
 			}
@@ -130,7 +166,7 @@ $arrJobs = array(
         //爬虫从开始页面算起，最多爬取的深度，设置为1表示只爬取起始页面
         //Crawler's max following depth, 1 stands for only crawl the start page
         'max_depth' => 2,
-	'max_page' => 5, 
+	'max_page' => 2, 
         
     ) ,   
 );
