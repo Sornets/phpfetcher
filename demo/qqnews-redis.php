7ff14e609950abd0825cd9c9b15f1082e4335a4b
suxici <hellosxc@163.com>
2016-05-16 18:12:17 +0800

    增加对redis的支持

---
+++ demo/qqnews-redis.php
@@ -0,0 +1,165 @@
+<?php
+//下面两行使得这个项目被下载下来后本文件能直接运行
+$demo_include_path = dirname(__FILE__) . '/../';
+set_include_path(get_include_path() . PATH_SEPARATOR . $demo_include_path);
+
+require_once('phpfetcher.php');
+require_once('lib.php');
+//mysql info
+$config = array(
+    'db_host'       => 'localhost',
+    'db_port'       => '3306',
+    'db_username'   => 'root',
+    'db_password'   => 'root',
+    'db_name'       => 'qqnews',//库名
+    'db_pre'        => '',//前缀
+);
+
+$db = new Phpfetcher_MySQL_Default( $config );
+$curl = curl_init();
+class mycrawler extends Phpfetcher_Crawler_Default {
+    public function handlePage($page) {
+    	$need_log = false;
+    	$error_type = "";
+
+    	$news_url = $page->getUrl();
+        //获取标题
+        $str_title = $page->sel('//h1', 0)->plaintext;
+		//获取文章内容
+        $arr_content = $page->sel('div[@id=Cnt-Main-Article-QQ]/p');
+        if( $arr_content ){
+			$str_content = '';//*
+        	foreach( $arr_content as $content ){
+				$str_content .= $content->plaintext;
+      	     	$str_content .= "<br />";
+			}
+
+			//获取文章id
+			$str_html = $page->getContent();
+			$str_cmt_id_patten = "#cmt_id\s?=\s?\d*;#";
+			$match_count = preg_match( $str_cmt_id_patten, $str_html, $matches );
+			if( empty( $match_count ) ){//cmt_id获取失败
+				//aid: "1400425094",
+				$str_aid_patten = "#aid:\s?\"\s?\d*\",#";
+				$match_count = preg_match( $str_aid_patten, $str_html, $matches );
+
+				if( empty( $match_count ) ){//aid 获取失败
+					$need_log = true;
+					$error_type .= "Match cmt_id & aid failed.";
+				}
+				else{//aid 获取成功
+					$matches[0] = str_replace( "aid: \"", "", $matches[0] );
+					$matches[0] = str_replace( "\",", "", $matches[0] );
+					$cmt_id = intval( $matches[0] );
+				}
+			}
+			else{//获取成功
+				$matches[0] = str_replace( "cmt_id = ", "", $matches[0] );
+				$matches[0] = str_replace( ";", "", $matches[0] );
+				$cmt_id = intval( $matches[0] );
+			}
+
+			if( $cmt_id ){
+				//将cmt_id保存到redis中
+				$this->_redis->rpush( 'need:crawled:news:ids', $cmt_id );
+			}
+			else{
+				$need_log = true;
+				$error_type .= "Get news id failed.";
+				$error_sql = "INSERT INTO `fail`( `err_type`, `content`) VALUES ( '$error_type', " . $news_url . "')";
+				$error_sql = mysql_real_escape_string($error_sql);
+				$GLOBALS['db']->exe_sql( $error_sql );
+			}
+
+			//获取新闻类型
+			@$str_type = $page->sel('span[@bosszone=ztTopic]/a', 0)->plaintext;
+			
+			//获取来源
+			$obj_refer = $page->sel('span[@bosszone=jgname]/a', 0);
+			if( empty( $obj_refer) ){
+				//获取失败
+				$obj_refer = $page->sel('span[@class=where]/a', 0);
+				if( empty( $obj_refer) ){
+					//两次获取都失败了
+					$need_log = true;
+					$error_type .= "Get refer info failed.";
+				}
+			}
+			$str_refer = $obj_refer->plaintext;
+			//获取来源域名
+			$arr_url = parse_url( $obj_refer->href );
+			$str_refer_url = $arr_url['scheme'] . "://" . $arr_url['host'];
+
+			//获取发布时间
+			$time = $page->sel('//span[@class=article-time]', 0)->plaintext;
+			if( empty( $time ) ){
+				//for http://news.qq.com/a/20120425/000744.htm
+				$time = $page->sel('//span[@class=pubTime]', 0)->plaintext;
+				if( !empty( $time ) ){
+					$time = str_replace( '年', '-', $time );
+					$time = str_replace( '月', '-', $time );
+					$time = str_replace( '日', ' ', $time );
+				}
+				else{
+					$need_log = true;
+					$error_type .= "Get public time failed.";
+				}
+			}
+
+			$time = strtotime( $time );
+						//保存信息
+			$db_name = $GLOBALS['db']->_db_name;
+			$db_pre = $GLOBALS['db']->_pre;
+
+			$sql = "INSERT INTO `news` ( `real_id`, `news_url`, `title`, `comment_num`, `content`, `refer`, `refer_url`, `news_type`, `time` ) VALUES ( '$cmt_id', '$news_url', '$str_title', '$int_comment', '$str_content', '$str_refer', '$str_refer_url', '$str_type', '$time' )";
+			$sql = mysql_real_escape_string($sql);
+			
+			if( !$GLOBALS['db']->exe_sql( $sql ) ){
+				//检查数据库中是否已经存在当前新闻
+				$str_has_sql = "SELECT id FROM `news` WHERE id='$str_title'";
+				$str_has_sql = mysql_real_escape_string($str_has_sql);
+				$has_this_news_handle = $GLOBALS['db']->exe_sql( $str_has_sql );
+				$has_this_news = mysql_fetch_assoc( $has_this_news_handle );
+				
+				//print_r( $has_this_user );
+				if( $has_this_news ){
+					//echo "id $user[userid] continued." . PHP_EOL;
+					return;
+				}
+			}
+
+			if( $need_log ){
+				$error_sql = "INSERT INTO `fail`( `err_type`, `content`) VALUES ( '$error_type', " . mysql_real_escape_string($sql) . "')";
+				$GLOBALS['db']->exe_sql( $error_sql );
+			}
+			
+		}//if( $arr_content )
+    }
+}
+
+$crawler = new mycrawler();
+$arrJobs = array(
+    //任务的名字随便起，这里把名字叫qqnews
+    //the key is the name of a job, here names it qqnews
+    'qqnews' => array( 
+        //'start_page' => 'http://news.qq.com/', //起始网页
+        'start_page' => 'http://news.qq.com/a/20160421/053219.htm', 
+        'link_rules' => array(
+            /*
+             * 所有在这里列出的正则规则，只要能匹配到超链接，那么那条爬虫就会爬到那条超链接
+             * Regex rules are listed here, the crawler will follow any hyperlinks once the regex matches
+             */
+            '#news\.qq\.com/a/\d+/\d+\.htm$#',
+        ),
+        //爬虫从开始页面算起，最多爬取的深度，设置为1表示只爬取起始页面
+        //Crawler's max following depth, 1 stands for only crawl the start page
+	//'max_depth' => 100,
+	//'max_pages' => 3, 
+    ) ,   
+);
+
+$redis = new Redis();
+$redis->connect( '127.0.0.1', 6379 );
+$crawler->setFetchJobs($arrJobs);
+$crawler->setRedis( $redis );//设置爬虫的redis信息
+$crawler->run();
